c51373aea56b8c54e4225dee1d70062bf5b82b4e
Michał Reszka <michal.reszka86@gmail.com>
2026-01-30 23:48:28 +0100

    Add MessageWindow usage as AboutWindow

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,22 +1,13 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@onready var about_window: Control = $AboutWindow	
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_about_pressed() -> void:
-	print("about placeholder")
+	about_window.open("About sample text")
 
 
 func _on_quit_pressed() -> void:
