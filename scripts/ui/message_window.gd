5b667c74ea1f113c5a47561d8589f53d60d2e046
Michał Reszka <michal.reszka86@gmail.com>
2026-01-30 23:47:10 +0100

    Modify how MessageWindow visibility is handled

--- scripts/ui/message_window.gd
+++ scripts/ui/message_window.gd
@@ -2,8 +2,12 @@ extends Control
 
 @onready var message_label: Label = $PopupBox/TextContainer/MessageLabel
 
+func _ready() -> void:
+	hide()
+
 func open(text: String):
 	message_label.text = text
+	show()
 
 func _on_close_button_pressed() -> void:
-	get_tree().quit(0)	
+	hide()
