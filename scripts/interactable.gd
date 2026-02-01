bb5c10cf3ad111da78a379ca443ced040fa831dc
rreszka <michal.reszka86@gmail.com>
2026-02-01 12:23:11 +0100

    text above interactable items

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -2,6 +2,10 @@ extends Area2D
 class_name Interactable
 
 @export var only_once = false
+@export var text = ''
+@export var text_x_offset = 0
+@export var text_y_offset = -15
+@onready var interactable_text: Label = $InteractableText
 
 signal interacted
 
@@ -9,12 +13,19 @@ func _ready():
 	for parent_group in get_parent().get_groups():
 		if parent_group.begins_with("show_in"):
 			add_to_group(parent_group)
-
+	
 func interact():
 	interacted.emit()
 	if (only_once):
 		get_parent().queue_free()
-		
+	
+func _draw() -> void:
+	if interactable_text:
+		interactable_text.text = text
+		var new_pos = Vector2(interactable_text.position)
+		new_pos.x += text_x_offset
+		new_pos.y += text_y_offset
+		interactable_text.set_position(new_pos)
 
 func can_interact():
 	if is_in_group("show_in_past") and MaskManager.current_mask != MaskManager.MASK.PAST:
